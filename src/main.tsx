598680edbd92a5d3e9183fb8c82f04eb5d85d94d
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-16 22:50:48 +0500

    Add BrowserRouter to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
@@ -7,4 +8,8 @@ const rootElement = ReactDOM.createRoot(
 	document.getElementById('root')!
 );
 
-rootElement.render(<App />);
+rootElement.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
