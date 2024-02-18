fcb9f2a18f66455c71fd795ae5e14e19738683e9
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-18 22:12:03 +0500

    Refactor API.ts file to include new functions for creating user account and saving user to database

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,11 @@
 import { ID } from 'appwrite';
 import { INewUser } from '@/types';
-import { account } from './config';
+import {
+	account,
+	appwriteConfig,
+	avatars,
+	database,
+} from './config';
 
 export async function CreateUserAccount(user: INewUser) {
 	try {
@@ -11,7 +16,46 @@ export async function CreateUserAccount(user: INewUser) {
 			user.name
 		);
 
-		return newAccount;
+		if (!newAccount) throw new Error('Account not created');
+
+		const avatarUrl = avatars.getInitials(user.name);
+
+		const newUser = await saveUserToDB({
+			accountId: newAccount.$id,
+			email: newAccount.email,
+			name: newAccount.name,
+			imageUrl: avatarUrl,
+			username: user.username,
+		});
+
+		return newUser;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function saveUserToDB(user: {
+	accountId: string;
+	email: string;
+	name: string;
+	imageUrl: URL;
+	username?: string;
+}) {
+	try {
+		const newUser = await database.createDocument(
+			appwriteConfig.databaseId,
+			appwriteConfig.userCollectionId,
+			ID.unique(),
+			{
+				accountId: user.accountId,
+				email: user.email,
+				name: user.name,
+				imageUrl: user.imageUrl,
+				username: user.username,
+			}
+		);
+
+		return newUser;
 	} catch (error) {
 		console.log(error);
 	}
