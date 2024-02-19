42e52f45913d7fb4d9b09fb10b5523853eeab082
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-19 19:31:55 +0500

    Fix: collection IDs in appwriteConfig

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -7,16 +7,16 @@ import {
 } from 'appwrite';
 
 export const appwriteConfig = {
-	projectId: import.meta.env.VITE_APP_APPWRITE_PROJECT_ID,
-	url: import.meta.env.VITE_APP_APPWRITE_ENDPOINT,
-	databaseId: import.meta.env.VITE_APP_APPWRITE_DATABASE_ID,
-	storageId: import.meta.env.VITE_APP_APPWRITE_STORAGE_ID,
+	projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+	url: import.meta.env.VITE_APPWRITE_ENDPOINT,
+	databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+	storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
 	userCollectionId: import.meta.env
-		.VITE_APP_APPWRITE_USER_COLLECTION_ID,
+		.VITE_APPWRITE_USERS_COLLECTION_ID,
 	postCollectionId: import.meta.env
-		.VITE_APP_APPWRITE_POST_COLLECTION_ID,
+		.VITE_APPWRITE_POSTS_COLLECTION_ID,
 	saveCollectionId: import.meta.env
-		.VITE_APP_APPWRITE_SAVE_COLLECTION_ID,
+		.VITE_APPWRITE_SAVES_COLLECTION_ID,
 };
 
 export const client = new Client();
