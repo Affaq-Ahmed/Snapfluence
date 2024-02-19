594f38cf2dcd00c0686b837822684f277edec8eb
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-19 18:24:36 +0500

    Add AuthProvider and QueryProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import AuthProvider from './context/AuthContext';
+import QueryProvider from './lib/react-query/queryProvider';
 
 const rootElement = ReactDOM.createRoot(
 	document.getElementById('root')!
@@ -10,6 +11,10 @@ const rootElement = ReactDOM.createRoot(
 
 rootElement.render(
 	<BrowserRouter>
-		<App />
+		<QueryProvider>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</QueryProvider>
 	</BrowserRouter>
 );
