ce4718c4abd33fdd0625b762be2beeeab090132a
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-22 22:08:21 +0500

    feat: Add useCreatePostMutation hook

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -7,9 +7,11 @@ import {
 import {
 	CreateUserAccount,
 	SigninAccount,
+	createPost,
 	signOutAccount,
 } from '../appwrite/api';
-import { INewUser } from '@/types';
+import { INewPost, INewUser } from '@/types';
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccountMutation = () => {
 	return useMutation({
@@ -29,5 +31,18 @@ export const useSignInAccountMutation = () => {
 export const useSignOutMutation = () => {
 	return useMutation({
 		mutationFn: signOutAccount,
+	});
+};
+
+export const useCreatePostMutation = () => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: (post: INewPost) => createPost(post),
+
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+			});
+		},
 	});
 };
