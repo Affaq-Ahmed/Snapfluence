e6e16019dc09a351f1b2c54aba518a17b5495630
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-23 13:26:15 +0500

    feat: Add recent posts to home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,7 +1,40 @@
+import Loader from '@/components/shared/Loader';
+import PostCard from '@/components/shared/PostCard';
+import { useGetRecentPosts } from '@/lib/react-query/queriesAndMutations';
+import { Models } from 'appwrite';
 import React from 'react';
 
 const Home = () => {
-	return <div>Home</div>;
+	const {
+		data: posts,
+		isPending: isPostLoading,
+		isError: isErrorPosts,
+	} = useGetRecentPosts();
+
+	return (
+		<div className='flex flex-1'>
+			<div className='home-container'>
+				<div className='home-posts'>
+					<h2 className='h3-bold md:h2-bold text-left w-full'>
+						Home Feed
+					</h2>
+					{isPostLoading && !posts ? (
+						<div className='flex justify-center items-center h-96'>
+							<Loader />
+						</div>
+					) : (
+						<ul className='flex flex-col flex-1 gap-9 w-full'>
+							{posts?.documents.map(
+								(post: Models.Document) => (
+									<PostCard post={post} key={post.$id} />
+								)
+							)}
+						</ul>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
