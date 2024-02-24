8470813cd4ee8a0582901c39047383097a58d384
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-24 17:50:44 +0500

    feat: Add new query keys for user, post, and search

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -1,14 +1,19 @@
 export enum QUERY_KEYS {
-	GET_RECENT_POSTS = 'getRecentPosts',
+	CREATE_USER_ACCOUNT = 'createUserAccount',
+
+	// USER KEYS
+	GET_CURRENT_USER = 'getCurrentUser',
+	GET_USERS = 'getUsers',
+	GET_USER_BY_ID = 'getUserById',
+
+	//POST KEYS
+	GET_POSTS = 'getPosts',
 	GET_USER_POSTS = 'getUserPosts',
-	GET_USER_LIKED_POSTS = 'getUserLikedPosts',
-	GET_USER_SAVED_POSTS = 'getUserSavedPosts',
-	GET_USER_FOLLOWING = 'getUserFollowing',
-	GET_USER_FOLLOWERS = 'getUserFollowers',
-	GET_USER_PROFILE = 'getUserProfile',
-	GET_POST = 'getPost',
-	GET_POST_COMMENTS = 'getPostComments',
-	GET_POST_LIKES = 'getPostLikes',
-	GET_POST_SAVES = 'getPostSaves',
+	GET_RECENT_POSTS = 'getRecentPosts',
+	GET_INFINTY_POSTS = 'getInfinityPosts',
 	GET_POST_BY_ID = 'getPostById',
+	GET_FILE_PREVIEW = 'getFilePreview',
+
+	//SEARCH KEYS
+	SEARCH_POSTS = 'searchPosts',
 }
