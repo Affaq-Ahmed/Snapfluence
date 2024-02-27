b9cc536184ab2c4da21173da5bd3dcc6f3e70925
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-27 19:20:55 +0500

    feat: Add updatePost and deletePost functions

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
 import { ID, Query } from 'appwrite';
-import { INewPost, INewUser } from '@/types';
+import { INewPost, INewUser, IUpdatePost } from '@/types';
 import {
 	account,
 	appwriteConfig,
@@ -283,6 +283,85 @@ export async function getPostById(postId: string) {
 		if (!post) throw new Error('Post not found');
 
 		return post;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function updatePost(post: IUpdatePost) {
+	const hasFile = post.file.length > 0;
+	try {
+		let image = {
+			imageUrl: post.imageUrl,
+			imageId: post.imageId,
+		};
+
+		if (hasFile) {
+			//Uplaod File first
+			const uploadedFile = await uploadFile(post.file[0]);
+
+			if (!uploadedFile) throw new Error();
+
+			const fileUrl = getFilePreview(uploadedFile.$id);
+
+			if (!fileUrl) {
+				await deleteFile(uploadedFile.$id);
+				throw new Error();
+			}
+
+			image = {
+				...image,
+				imageUrl: fileUrl,
+				imageId: uploadedFile.$id,
+			};
+		}
+
+		//Convert tags to array
+		const tags =
+			post.tags?.replace(/ /g, '').split(',') || [];
+
+		const updatedPost = await database.updateDocument(
+			appwriteConfig.databaseId,
+			appwriteConfig.postCollectionId,
+			post.postId,
+			{
+				caption: post.caption,
+				imageUrl: image.imageUrl,
+				imageId: image.imageId,
+				location: post.location,
+				tags: tags,
+			}
+		);
+
+		if (!updatedPost) {
+			await deleteFile(post.imageId);
+			throw new Error('Post not created');
+		}
+
+		return updatedPost;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function deletePost(
+	postId: string,
+	imageId: string
+) {
+	if (!postId || !imageId) throw Error;
+
+	try {
+		const deletedPost = await database.deleteDocument(
+			appwriteConfig.databaseId,
+			appwriteConfig.postCollectionId,
+			postId
+		);
+
+		if (!deletedPost) throw new Error('Post not deleted');
+
+		await deleteFile(imageId);
+
+		return { status: 'ok' };
 	} catch (error) {
 		console.log(error);
 	}
